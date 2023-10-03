8d4fef31853b7eb321abdea421b02a95c58b985f
mohirdi <114629894+mohirdi@users.noreply.github.com>
2023-10-03 19:44:45 +0700

    Add files via upload

---
+++ sesi2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+
+class Main {
+  public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Masukkan bilangan: ");
+        int bilangan = scanner.nextInt();
+
+        int hasil = bilangan * bilangan;
+        System.out.println("Hasil pangkat dua dari " + bilangan + " adalah: " + hasil);
+
+        
+    }
+
+    public static int hitungPangkatDua(int bilangan) {
+        int hasil = 1;
+        for (int i = 0; i < bilangan; i++) {
+            hasil *= 2;
+        }
+
+        return hasil;
+    
+}
+
+}
